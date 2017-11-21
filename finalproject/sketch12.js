f5944224f26755b99abb545a1b24788cfbfd99aa
lepstein6 <lepstein@mit.edu>
2017-11-21 14:30:03 -0500

    Update sketch12.js

--- finalproject/sketch12.js
+++ finalproject/sketch12.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.ajax({
         type: "GET",
-        url: "data/runningRecordUVW_test.csv",
+        url: "data/BCV_runningRecord.csv",
         dataType: "text",
         success: function(data) {parseData(data);}
      });
